50ebfb5dfc6aeb9c3b93fb0e506ef7bccf89ff34
vladimirr11 <vladimirstoevv@gmail.com>
2021-02-09 15:31:26 +0200

    Programming basics with Python.

---
+++ programming-basics-with-python/intro-in-programming-with-python/lab09_project_creation.py
@@ -0,0 +1,6 @@
+architect_name = input()
+num_projects = int(input())
+
+TIME_PROJECT = 3
+
+print(f'The architect {architect_name} will need {num_projects * TIME_PROJECT} hours to complete {num_projects} project/s.')
