1e32d85877a6bc12217295fdd1ca73996380e698
vladimirr11 <vladimirstoevv@gmail.com>
2021-03-03 23:08:38 +0200

    Python fundamentals.

---
+++ python-fundamentals/data-types-and-variables/lab4_convert_meters_to_kilometers.py
@@ -0,0 +1,6 @@
+METERS_IN_KILOMETER = 1_000
+
+meters = int(input())
+kilometers = meters / METERS_IN_KILOMETER
+
+print(f'{kilometers:.2f}')
---
+++ python-fundamentals/data-types-and-variables/lab5_pounds_to_dollars.py
@@ -0,0 +1,5 @@
+GBP_TO_USD_RATE = 1.31
+pounds = float(input())
+dollars = pounds * GBP_TO_USD_RATE
+
+print(f'{dollars:.3f}')
