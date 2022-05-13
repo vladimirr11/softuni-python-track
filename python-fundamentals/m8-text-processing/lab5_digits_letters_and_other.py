c499820d1b86e0f0c8cf37e76a4fd52d32736c8e
Vladimir Stoev <vladimirstoevv@gmail.com>
2022-05-13 17:34:09 +0300

    Added lab and exercise for text processing.

---
+++ python-fundamentals/m8-text-processing/lab5_digits_letters_and_other.py
@@ -0,0 +1,40 @@
+text = input()
+
+
+def get_all_digits(text):
+    """"
+    Take out all digits from given text.
+    """
+    return get_all_character_matching_condition(text, lambda char: char.isdigit())
+
+
+def get_all_letters(text):
+    """"
+    Take out all letters from given text.
+    """
+    return get_all_character_matching_condition(text, lambda char: char.isalpha())
+
+
+def get_all_other_characters(text):
+    """"
+    Take out all non-numeric and all non-alphabetic character from given text.
+    """
+    return get_all_character_matching_condition(text, lambda char: not char.isdigit() and not char.isalpha())
+
+
+def get_all_character_matching_condition(text, condition):
+    """
+    Take out all character provided given condition.
+    """
+    result = ''
+
+    for char in text:
+        if condition(char):
+            result += char
+
+    return result
+
+
+print(get_all_digits(text))
+print(get_all_letters(text))
+print(get_all_other_characters(text))
